1857a8df680fc80400e9a4556fcf752986d727f4
TechieRocky <34655689+vilaswasnik@users.noreply.github.com>
2025-04-26 17:34:24 +0000

    Moved code to vilas branch

---
+++ gitpushtoMain.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "Moved code to main branch"
+git push origin main
+git branch -a
---
+++ gitpushtovilas.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "Moved code to vilas branch"
+git push origin vilas
+git branch -a
