074e1ba9a2ada865abb3e1769d79b3f687d20373
TechieVilsy <34655689+vilaswasnik@users.noreply.github.com>
2025-08-27 18:04:09 +0000

    chore: Remove gitpushtovilas.sh script
    - Delete unnecessary git push script
    - Clean up project directory

--- gitpushtovilas.sh
+++
@@ -1,6 +0,0 @@
-# git checkout -b develop  - this tis to create 
-# git checkout develop  - this is to switch  branch
-git add .
-git commit -m "Moved code to vilas branch 4 29"
-git push origin vilas
-git branch -a
